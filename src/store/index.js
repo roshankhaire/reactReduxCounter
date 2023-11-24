c850b5dec22ae2d0e1c1383179e304272fc7f1b1
roshankhaire <roshankhaire1992@gmail.com>
2023-11-24 18:14:10 +0530

    login Logout redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,42 @@
-import {createStore} from "redux";
-const counterReducer=(state={counter:0},action)=>{
-    if(action.type === 'increment'){
-        return{
-            counter:state.counter+5,
+
+import {createSlice,configureStore} from '@reduxjs/toolkit'
+const initialCounterState={counter:0,showCounter:true}
+    const couterSlice= createSlice({
+        name:'counter',
+        initialState:initialCounterState,
+        reducers:{
+            increment(state) {
+                state.counter++;
+            },
+            decrement(state) {
+                state.counter--;
+            },
+            increase(state,action) {
+                state.counter=state.counter+action.payload
+            },
+            toggleCounter(state){
+                state.showCounter=!state.showCounter
+            }
         }
-    }
-    if(action.type === 'decrement'){
-        return{
-            counter:state.counter-5,
+       })
+       const initialAuthState={
+        isAuthenticated:false
+       }
+       const authSlice=createSlice({
+        name:"authentication",
+        initialState:initialAuthState,
+        reducers:{
+            login(state) {
+                state.isAuthenticated=true
+            },
+            logout(state) {
+                state.isAuthenticated=false
+            }
         }
-    }
-    return state
-}
-const store=createStore(counterReducer)
+       })
+const store=configureStore({
+    reducer:{counter:couterSlice.reducer,auth:authSlice.reducer},
+})
+export const counterActions=couterSlice.actions
+export const authActions=authSlice.actions
 export default store
